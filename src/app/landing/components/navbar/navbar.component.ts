f1abe6a05d04873ece5d4c0d07154ae9abff88d3
jlinares <jorgelinaresrrs301@gmail.com>
2025-07-08 16:38:42 -0500

    link modified

--- src/app/landing/components/navbar/navbar.component.ts
+++ src/app/landing/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  menuOpen: boolean = false;
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
